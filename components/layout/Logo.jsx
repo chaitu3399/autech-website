737bb46ca932f96d2d33b4301c5c8d0bab441991
Chaitanya <chaitanya@octosol.in>
2026-05-28 13:04:33 +0530

    removed few things

--- components/layout/Logo.jsx
+++ components/layout/Logo.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <span className="inline-flex items-center" aria-label="Autech LLC">
-      <Image src="/logo-wordmark.png" alt="Autech" width={180} height={44} className="h-7 w-auto" priority />
+      <Image src="/logo-wordmark.png" alt="Autech" width={240} height={56} className="h-10 w-auto" priority />
     </span>
   );
 }
